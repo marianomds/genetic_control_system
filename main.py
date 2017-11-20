6637da6107b960cfd92aa644ea712712fa9d3736
Mariano <mmarufodasilva@est.frba.utn.edu.ar>
2017-11-20 19:45:56 -0300

    Added generation of input signal and replaced step response evaluation with lsim.

--- main.py
+++ main.py
@@ -5,18 +5,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import zpk2tf 
 
-# Default time vector parameters
+# Time vector parameters
 START_TIME = 0
 STOP_TIME = 20
 STEP_TIME = 0.01
 
-# Default limit values for controller parameters
+# Input parameters
+FINAL_VALUE = 2
+FINAL_TIME = 10 # MUST BE: FINAL_TIME < STOP_TIME
+
+# Limit values for controller parameters
 K_MAX = 100
 ZERO_MIN = -20
 
-# Default threshold values for algorithm convergence
+# Threshold values for algorithm convergence
 OVERSHOOT_MAX = 20 # in percentage
-RISE_TIME_MAX = 3 # in seconds
+RISE_TIME_MAX = 11 # MUST BE: RISE_TIME_MAX > FINAL_TIME
+FINAL_ERROR_MAX = 0.05
+
+def create_input():
+    input_signal = np.arange(START_TIME, FINAL_TIME, STEP_TIME)*(FINAL_VALUE/FINAL_TIME)
+    input_signal = np.append(input_signal, np.ones(int((STOP_TIME - FINAL_TIME)/STEP_TIME))*FINAL_VALUE)
+    return input_signal
 
 def overshoot(signal):
     return (signal.max() - signal[-1])*100
@@ -29,7 +39,7 @@ def evolution(Gp, Time):
 
     ov = 999
     rt = 999
-    while ((ov > OVERSHOOT_MAX) or (rt > RISE_TIME_MAX)):
+    while ((ov > OVERSHOOT_MAX) or (rt > RISE_TIME_MAX)): # agregar evaluar por error max
 
         K = np.random.uniform(0, K_MAX)
         Z = np.random.uniform(ZERO_MIN, 0)
@@ -47,7 +57,7 @@ def evolution(Gp, Time):
         M = ctrl.feedback(Gc*Gp,1)
 
         # Closed loop step response
-        (y,t) = ctrl.step(M, Time)
+        y, t, xout = ctrl.lsim(M, input_signal, T)
 
         # Evaluate overshoot and rise time
         ov = overshoot(y)
@@ -79,22 +89,24 @@ if __name__ == "__main__":
     # Create time vector to be used
     T = np.arange(START_TIME, STOP_TIME, STEP_TIME)
 
-    # Generate input and plant step response
-    (y1,t1) = ctrl.step(ctrl.tf([1, 0],[1, 0]), T) # with ([1],[1]) step throws an error
-    (y2,t2) = ctrl.step(Gp, T)
+    # Generate input signal
+    input_signal = create_input()
+
+    # Generate plant response to input signal
+    y1, t1, xout = ctrl.lsim(Gp, input_signal, T)
 
     # Run evolution algorithm
     Gc, M = evolution(Gp, T)
 
-    # Closed loop step response
-    (y3,t3) = ctrl.step(M, T)
+    # Closed loop response to input signal
+    y2, t2, xout = ctrl.lsim(M, input_signal, T)
 
     # Evaluate and print overshoot and rise time
-    print('Overshoot: %f %%' % overshoot(y3))
-    print('Rise time: %1.2f sec' % rise_time(y3))
+    print('Overshoot: %f %%' % overshoot(y2))
+    print('Rise time: %1.2f sec' % rise_time(y2))
 
     # Plot result
-    plt.plot(t1, y1, t2, y2, t3, y3)
+    plt.plot(T, input_signal, t1, y1, t2, y2)
     plt.draw()
 
     # Block until the plot window is closed
