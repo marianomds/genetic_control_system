eebc664b3563066cb81d8f00f4ffd6beb5d62994
Mariano <mmarufodasilva@est.frba.utn.edu.ar>
2017-12-10 14:47:31 -0300

    Created individual class

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ OVERSHOOT_TH = 3 # in percentage
 RISE_TIME_TH = 11
 MSE_TH = 0.01
 
+# Genetic algorithm parameters
+POPULATION_SIZE = 10
+MAX_GEN = 10 # maximum number of generations
+CROSS_OVER_P = 0.5 # probability of crossing over
+
+
 def sigmoid (x):
     return 1/(1 + np.exp(-x))
 
@@ -76,13 +82,49 @@ def evaluate(x, y):
         print('Incorrect optimization metric.')
         quit()
 
+class individual():
+
+    # Initialization funcion
+    def __init__(self, Gp, Time, Input):
+        while(True):
+            # Try random parameter values
+            self.Z1 = np.random.uniform(ZERO_MIN, 0)
+            self.Z2 = np.random.uniform(ZERO_MIN, 0)
+
+            # Create test PI controller (used to calculate the maximum K for closed loop stable)
+            (self.Gc_num,self.Gc_den) = zpk2tf([self.Z1, self.Z2],[0],1) # PID controller, 3 parameters: location of 2 zeros, value of K, (+ 1 pole always in origin)
+            self.Gc = ctrl.tf(self.Gc_num,self.Gc_den)
+
+            # Evaluate closed loop stability
+            gm, pm, Wcg, Wcp = ctrl.margin(self.Gc*Gp)
+
+            # Dischard solutions with no gain margin
+            if gm == None:
+                continue
+
+            # If K < gm => closed loop stable (gm > 0dB)
+            self.K = np.random.uniform(0, gm)
+
+            # Create PI controller for closed loop stable system
+            (self.Gc_num,self.Gc_den) = zpk2tf([self.Z1, self.Z2],[0],self.K) # PID controller, 3 parameters: location of 2 zeros, value of K, (+ 1 pole always in origin)
+            self.Gc = ctrl.tf(self.Gc_num,self.Gc_den)
+
+            # Closed loop system
+            self.M = ctrl.feedback(self.Gc*Gp,1)
+
+            # Closed loop step response
+            y, t, xout = ctrl.lsim(self.M, Input, Time)
+
+            # Evaluate fitness
+            self.fitness = evaluate(Input,y)
+
+            break
+
 def evolution(Gp, Time, Input):
 
     # Variable for storing best fitness
-    fitness_best = 999 # Initial large enough value to ensure entering while loop
+#    fitness_best = 999 # Initial large enough value to ensure entering while loop
 
-    # Number of not closed loop stable tries discarded 
-    margin_discarded = 0
 
     # Select fitness threshold depending on metric to optimize
     if OPTIMIZE == 'OV':
@@ -95,58 +137,37 @@ def evolution(Gp, Time, Input):
         print('Incorrect optimization metric.')
         quit()
     
+    # Population: list of individual objects
+    population = []
+
     # Array for storing history of best fitness individuals
     fitness_best_vec = np.array([])
+
+    # Create random population
+    for count in range(POPULATION_SIZE):
+        x = individual(Gp, Time, Input)
+        population.append(x)
+
+    # Create random best individual
+    best = individual(Gp, Time, Input)
 
     # Number of loops
     loop_n = 0
 
     # Keep entering while loop until fitness threshold is reached
-    while (fitness_best > fitness_th):
+    while (loop_n < MAX_GEN and best.fitness > fitness_th):
 
         loop_n += 1
 
-        # Try random parameter values
-        Z1 = np.random.uniform(ZERO_MIN, 0)
-        Z2 = np.random.uniform(ZERO_MIN, 0)
-
-        # Create test PI controller (used to calculate the maximum K for closed loop stable)
-        (Gc_num,Gc_den) = zpk2tf([Z1, Z2],[0],1) # PID controller, 3 parameters: location of 2 zeros, value of K, (+ 1 pole always in origin)
-        Gc = ctrl.tf(Gc_num,Gc_den)
-
-        # Evaluate closed loop stability
-        gm, pm, Wcg, Wcp = ctrl.margin(Gc*Gp)
-
-        # Dischard solutions with no gain margin
-        if gm == None:
-            margin_discarded += 1
-            continue
-
-        # If K < gm => closed loop stable (gm > 0dB)
-        K = np.random.uniform(0, gm)
-
-        # Create PI controller for closed loop stable system
-        (Gc_num,Gc_den) = zpk2tf([Z1, Z2],[0],K) # PID controller, 3 parameters: location of 2 zeros, value of K, (+ 1 pole always in origin)
-        Gc = ctrl.tf(Gc_num,Gc_den)
-
-        # Closed loop system
-        M = ctrl.feedback(Gc*Gp,1)
-
-        # Closed loop step response
-        y, t, xout = ctrl.lsim(M, Input, Time)
-
-        # Evaluate fitness
-        fitness = evaluate(Input,y)
-
-        # If better fitness value is found, save (best) parameters
-        if fitness < fitness_best:
-            fitness_best = fitness
-            K_best = K
-            Z1_best = Z1
-            Z2_best = Z2
+#        # If better fitness value is found, save (best) parameters
+#        if fitness < fitness_best:
+#            fitness_best = fitness
+#            K_best = K
+#            Z1_best = Z1
+#            Z2_best = Z2
 
         # Save history of best fitness in a vector for plotting
-        fitness_best_vec = np.append(fitness_best_vec, fitness_best)
+        fitness_best_vec = np.append(fitness_best_vec, best.fitness)
 
         # Real time plot of history of best fitness
         plt.subplot(1,2,1)
@@ -154,20 +175,19 @@ def evolution(Gp, Time, Input):
         plt.pause(0.001)
 
     # Create best PI controller
-    (Gc_num_best,Gc_den_best) = zpk2tf([Z1_best, Z2_best],[0],K_best) # PI controller, 2 parameters: location of 1 zero, value of K, (+ 1 pole always in origin)
+    (Gc_num_best,Gc_den_best) = zpk2tf([best.Z1, best.Z2],[0],best.K) # PI controller, 2 parameters: location of 1 zero, value of K, (+ 1 pole always in origin)
     Gc_best = ctrl.tf(Gc_num_best,Gc_den_best)
 
     # Best closed loop system
     M_best = ctrl.feedback(Gc_best*Gp,1)
 
-    print('Total number of evaluations: %d' % loop_n)
-    print('Number of discarded (not closed loop stable): %d' % margin_discarded)
+    print('Total number of generations: %d' % loop_n)
 
     # Print controller information
     print('\nController:')
-    print('k: %f' % K_best)
-    print('zero 1: %f' % Z1_best)
-    print('zero 2: %f' % Z2_best)
+    print('k: %f' % best.K)
+    print('zero 1: %f' % best.Z1)
+    print('zero 2: %f' % best.Z2)
     print(Gc_best)
 
     # Print closed loop transfer function
