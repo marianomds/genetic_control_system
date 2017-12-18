027336be1758726e817d9e66d64a4dc868c77180
Mariano <mmarufodasilva@est.frba.utn.edu.ar>
2017-12-17 23:17:17 -0300

    Added SQL script for database creation.

---
+++ GeneticPID.sql
@@ -0,0 +1,10 @@
+/************************************
+   Drop database if it exists
+*************************************/
+DROP DATABASE IF EXISTS `GeneticPID`;
+
+
+/************************************
+   Create database
+*************************************/
+CREATE DATABASE `GeneticPID`;
